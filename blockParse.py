fb89e28801001a8fb9f2a1c8686e3544c68d6c4c
pediatricly <mdscahill@gmail.com>
2016-01-18 16:03:39 -0800

    loop through R3s works. fixing re errors with other classes then building output

--- blockParse.py
+++ blockParse.py
@@ -12,8 +12,8 @@ Plan (not flow)
 #- At some point, use str.isalpha() to strip the =,-,* crap off AmionName
 #
 #Then, for reals - ponder the data structure you want. Maybe:
-#{name : AmionN-F,
-# schedule : [
+#{AmionN-F :
+#  schedule : [
 #    {block : 9,
 #     startDate : 2016,3,7,
 #     stopDate : 2016,3,13,
@@ -22,11 +22,13 @@ Plan (not flow)
 #     startDate: 2016,3,14,
 #     stop...
 #    }],
-# CoC :
+#  CoC :
 #     {weekday : 2
 #     weekdayStr : 'Wed'
 #     time: 'pm'
-#     location: 'CARDS'}
+#     location: 'CARDS'},
+# Amion2-F :
+#  schedule :...
 #}
 #
 #This setup preserves an index for sub-rotations because they are in a list.
@@ -68,12 +70,14 @@ import datetime as DT
 #################################################################################
 
 week = dict(zip('Mon Tue Wed Thu Fri Sat Sun'.split(), range(7)))
-blockHTML = 'blockR3.html'
+blockHTML = 'blockR1.html'
 fh = open(blockHTML, 'rb')
 html = fh.read()
 fh.close()
 blockStarts = {}
 blockStops = {}
+blockLens = {}
+allRes = {}
 
 target = '^<TR.+?</tr>'
 TDtar = '<td.+?</td>'
@@ -120,12 +124,15 @@ def rowParser(seList):
 
 #################################################################################
 # Find the years from the Amion block page
-yearTar = 'Schedule, (\d\d\d\d).(\d\d\d\d)'
+yearTar = '(\d\d\d\d).(\d\d\d\d)'
 years = re.search(yearTar, html, re.I)
 fallYr = int(years.group(1))
 springYr = int(years.group(2))
 
+classTar = 'R(\d)\w+Block'
+classR = int(re.search(classTar, html, re.I).group(1))
 # re to find the whole table (assumes there's only 1 on the html page
+
 table = re.findall(target, html, re.M)
 header = table[0]
 main = table[1:]
@@ -162,60 +169,72 @@ for cell in headDates:
     else: yearI = springYr
     blockStops[block] = DT.date(yearI, stopNums[0], stopNums[1])
 
+lastBlock = max(blockStarts.keys())
+print lastBlock
+
+for block in blockStarts:
+    bLen = blockStops[block] - blockStarts[block]
+    bLen = round(bLen.days) + 1
+    blockLens[block] = bLen
+print blockLens
+
 #################################################################################
 ### Read the main (residents) part of the table
 #################################################################################
 
-# for line in main:
+for line in main:
 # This is where to build the loop for all resident lines eventually
-line = main[7]
+    # line = main[7]
 
 # Grabs all the TD tags (cells) in the given row
-se2 = re.findall(TDtar, line, re.I)
+    se2 = re.findall(TDtar, line, re.I)
 
 # Parses those TD tags into a list of block (cells) like the eg below
-rowListI = rowParser(se2)
-'''
+    rowListI = rowParser(se2)
+    '''
 # for cell in rowListI:
-    # print cell
-List of the table row's cells where multipart cells are list items.
-Ainsworth-A=
-E-CICU
-PICU3
-SFN3
-['VAC', '| SFX']
-SFO3
-SFO3
-PURPLE3
-PURPLE3
-['VAC', '3/7-3/13', 'JEOP | E-Pulm']
-['CHO-ICU', '4/4-4/10', 'JEOP | CARDS']
-['VAC', '5/2-5/8', 'CHO-ICU']
-ICN3
-Chief
-'''
+        # print cell
+    List of the table row's cells where multipart cells are list items.
+    Ainsworth-A=
+    E-CICU
+    PICU3
+    SFN3
+    ['VAC', '| SFX']
+    SFO3
+    SFO3
+    PURPLE3
+    PURPLE3
+    ['VAC', '3/7-3/13', 'JEOP | E-Pulm']
+    ['CHO-ICU', '4/4-4/10', 'JEOP | CARDS']
+    ['VAC', '5/2-5/8', 'CHO-ICU']
+    ICN3
+    Chief
+    '''
 
 # Pop off the AmionName from the first cell of the row
-AmionName = rowListI.pop(0)
-if AmionName[-1].isalpha() == False:
-    AmionName = AmionName[:-1]
-
+    AmionName = rowListI.pop(0)
+    if AmionName[-1].isalpha() == False:
+        AmionName = AmionName[:-1]
+    if AmionName[0].isalpha() == False: continue
 # Pop off & parse to dict the Coc date from the last cell
-CoCRaw = rowListI.pop(13)
-CoCweekDayStr = CoCRaw[:3]
-CoCTime = CoCRaw[3:5]
-CoCLoc = CoCRaw[5:]
-CoCweekDay = week[CoCweekDayStr]
+    CoCRaw = rowListI.pop(13)
+    CoCweekDayStr = CoCRaw[:3]
+    CoCTime = CoCRaw[3:5]
+    CoCLoc = CoCRaw[5:]
+    try: CoCweekDay = week[CoCweekDayStr]
+    except KeyError: CoCweekDay = None
 
 # Setup data structures for ea resident/row of the table
-CoCDict = {'weekday' : CoCweekDay,
-           'weekdayStr' : CoCweekDayStr,
-           'time' : CoCTime,
-           'location' : CoCLoc}
-resDict = {AmionName : {
-           'schedule' : [],
-           'CoC' : CoCDict}}
-schedule = []
+    CoCDict = {'weekday' : CoCweekDay,
+            'weekdayStr' : CoCweekDayStr,
+            'time' : CoCTime,
+            'location' : CoCLoc}
+    # resDict = {AmionName : {
+    resDict = {
+                'pgy' : classR,
+            'schedule' : [],
+            'CoC' : CoCDict}
+    schedule = []
 
 #################################################################################
 ### This super complex loop parses that list of cells into a list of dicts with
@@ -231,103 +250,106 @@ schedule = []
 # - Thus the bottoms have to adjust their start/stop dates to accomodate the
 # tops.
 
-for i, item in enumerate(rowListI):
-    # Setup empty dicts for each cell/item in the row
-    schedDict = {}
-    schedDictB = {}
-    schedDictB1 = {}
-    schedDictB2 = {}
+    for i, item in enumerate(rowListI):
+        # Setup empty dicts for each cell/item in the row
+        schedDict = {}
+        schedDictB = {}
+        schedDictB1 = {}
+        schedDictB2 = {}
 
 # Parse the easy, single rotation blocks - they are plain strings in rowList
-    if type(item) == str:
-        schedDict['block'] = i+1
-        schedDict['startDate'] = blockStarts[i+1]
-        try:
-            schedDict['stopDate'] = blockStops[i+1]
+        if type(item) == str:
+            schedDict['block'] = i+1
+            schedDict['startDate'] = blockStarts[i+1]
+            try:
+                schedDict['stopDate'] = blockStops[i+1]
 # Corrects lookup for the last block by going to the end of the year
-        except KeyError:
-            schedDict['stopDate'] = blockStops[13] + DT.timedelta(days=1)
-        schedDict['rotation'] = item
-        schedDict['bottom'] = 1 # All these are "bottoms" ie default rotations
+            except KeyError:
+                schedDict['stopDate'] = blockStops[lastBlock] + DT.timedelta(days=1)
+            schedDict['rotation'] = item
+            schedDict['bottom'] = 1 # All these are "bottoms" ie default rotations
 
 # cells that are list are multirotation blocks. May be just split bottoms or
 # bottom and top(s)
-    elif type(item) == list:
-        length = len(item)
-        lastTop = ''
-        # Find the dates that mark the line between tops & bottoms
-        for j, string in enumerate(item):
-            if string[0].isdigit() == True and string[-1].isdigit() == True:
-                lastTop = int(j)
-        # If there is a top line, parse it & save bottom for loop below
-        if lastTop != '':
-            tops = item[:lastTop+1]
-            bottomRow = item[lastTop+1:]
+        elif type(item) == list:
+            length = len(item)
+            lastTop = ''
+            # Find the dates that mark the line between tops & bottoms
+            for j, string in enumerate(item):
+                if string[0].isdigit() == True and string[-1].isdigit() == True:
+                    lastTop = int(j)
+            # If there is a top line, parse it & save bottom for loop below
+            if lastTop != '':
+                tops = item[:lastTop+1]
+                bottomRow = item[lastTop+1:]
 
-            # Parse the tops into a dict
-            for j, other in enumerate(tops):
-                schedDictO = {}
-                if other[0].isdigit() == True:
-                    weekRot = item[j-1]
-                    dates = other.split('-')
-                    startDO = dates[0]
-                    startDOs = startDO.split('/')
-                    startDOmo = int(startDOs[0])
-                    if startDOmo > 6: startDOYr = fallYr
-                    else: startDOYr = springYr
-                    startDOday = int(startDOs[1])
+                # Parse the tops into a dict
+                for j, other in enumerate(tops):
+                    schedDictO = {}
+                    if other[0].isdigit() == True:
+                        weekRot = item[j-1]
+                        dates = other.split('-')
+                        startDO = dates[0]
+                        startDOs = startDO.split('/')
+                        startDOmo = int(startDOs[0])
+                        if startDOmo > 6: startDOYr = fallYr
+                        else: startDOYr = springYr
+                        startDOday = int(startDOs[1])
 
-                    stopDO = dates[1]
-                    stopDOs = stopDO.split('/')
-                    stopDOmo = int(stopDOs[0])
-                    if stopDOmo > 6: stopDOYr = fallYr
-                    else: stopDOYr = springYr
-                    stopDOday = int(stopDOs[1])
-                    schedDictO['block'] = i+1
-                    schedDictO['startDate'] = DT.date(startDOYr, startDOmo,
-                                                      startDOday)
-                    schedDictO['stopDate'] = DT.date(stopDOYr, stopDOmo,
-                                                     stopDOday)
-                    schedDictO['rotation'] = weekRot
-                    schedDictO['bottom'] = 0
-                    if schedDictO != {}:
-                        schedule.append(schedDictO)
-        # If there's no top line, the whole cell, [a list], is the bottom row
-        else: bottomRow = item
+                        try: stopDO = dates[1]
+                        except: continue
+                        stopDOs = stopDO.split('/')
+                        stopDOmo = int(stopDOs[0])
+                        if stopDOmo > 6: stopDOYr = fallYr
+                        else: stopDOYr = springYr
+                        stopDOday = int(stopDOs[1])
+                        schedDictO['block'] = i+1
+                        schedDictO['startDate'] = DT.date(startDOYr, startDOmo,
+                                                        startDOday)
+                        schedDictO['stopDate'] = DT.date(stopDOYr, stopDOmo,
+                                                        stopDOday)
+                        schedDictO['rotation'] = weekRot
+                        schedDictO['bottom'] = 0
+                        if schedDictO != {}:
+                            schedule.append(schedDictO)
+            # If there's no top line, the whole cell, [a list], is the bottom row
+            else: bottomRow = item
 
-        # Parse the bottom row. Start by concatenating if not already
-        if len(bottomRow) > 1: bottom = ' '.join(bottomRow)
-        else: bottom = bottomRow[0]
+            # Parse the bottom row. Start by concatenating if not already
+            if len(bottomRow) > 1: bottom = ' '.join(bottomRow)
+            else: bottom = bottomRow[0]
 # If it is a split bottom, needs 2 dicts
-        if '|' in bottom:
-            bottoms = bottom.split(' | ')
-            schedDictB1['block'] = i+1
-            schedDictB1['startDate'] = blockStarts[i+1]
-            schedDictB1['stopDate'] = blockStarts[i+1] + DT.timedelta(days=13)
-            schedDictB1['rotation'] = bottoms[0]
-            schedDictB1['bottom'] = 1
-            schedDictB2['block'] = i+1
-            schedDictB2['startDate'] = blockStarts[i+1] + DT.timedelta(days=14)
-            schedDictB2['stopDate'] = blockStarts[i+2] + DT.timedelta(days=-1)
-            schedDictB2['rotation'] = bottoms[1]
-            schedDictB2['bottom'] = 1
-            schedule.append(schedDictB1)
-            schedule.append(schedDictB2)
-            item.pop(length - 1)
+            if '|' in bottom:
+                bottoms = bottom.split(' | ')
+                schedDictB1['block'] = i+1
+                schedDictB1['startDate'] = blockStarts[i+1]
+                schedDictB1['stopDate'] = blockStarts[i+1] + DT.timedelta(
+                    days=(blockLens[i+1]/2 -1))
+                schedDictB1['rotation'] = bottoms[0]
+                schedDictB1['bottom'] = 1
+                schedDictB2['block'] = i+1
+                schedDictB2['startDate'] = blockStarts[i+1] + DT.timedelta(
+                    days=blockLens[i+1]/2)
+                schedDictB2['stopDate'] = blockStops[i+1]
+                schedDictB2['rotation'] = bottoms[1]
+                schedDictB2['bottom'] = 1
+                schedule.append(schedDictB1)
+                schedule.append(schedDictB2)
+                item.pop(length - 1)
 # Non-split bottoms get a simple parsing
-        else:
-            schedDictB['block'] = i+1
-            schedDictB['startDate'] = blockStarts[i+1]
-            schedDictB['stopDate'] = blockStops[i+1]
-            schedDictB['rotation'] = bottom
-            schedDictB['bottom'] = 1
-            schedule.append(schedDictB)
-    if schedDict != {}:
-        schedule.append(schedDict)
+            else:
+                schedDictB['block'] = i+1
+                schedDictB['startDate'] = blockStarts[i+1]
+                schedDictB['stopDate'] = blockStops[i+1]
+                schedDictB['rotation'] = bottom
+                schedDictB['bottom'] = 1
+                schedule.append(schedDictB)
+        if schedDict != {}:
+            schedule.append(schedDict)
 
 # print len(resDict[AmionName]['schedule'])
 # for rot in  resDict[AmionName]['schedule']:
-    # print rot
+        # print rot
 
 #################################################################################
 ### Finally, adjust the bottoms' dates by the 'tops take precendence, bottoms
@@ -345,63 +367,91 @@ for i, item in enumerate(rowListI):
 
 # Start by grabbing all tops & bottoms (could have done this above but it's
 # messy enough)
-rowTops = []
-rowBottoms = []
-for rotation in schedule:
-    if rotation['bottom'] == 1:
-        rowBottoms.append(rotation)
-    elif rotation['bottom'] == 0:
-        rowTops.append(rotation)
+    rowTops = []
+    rowBottoms = []
+    for rotation in schedule:
+        if rotation['bottom'] == 1:
+            rowBottoms.append(rotation)
+        elif rotation['bottom'] == 0:
+            rowTops.append(rotation)
 
-for bottom in rowBottoms:
-    blockI = bottom['block']
-    botStart = bottom['startDate']
-    botStop = bottom['stopDate']
-    cellTops = []
-    topStarts = set()
-    topStops = set()
-    for top in rowTops:
-        if top['block'] == blockI:
-            cellTops.append(top)
-            topStarts.add(top['startDate'])
-            topStops.add(top['stopDate'])
+    for bottom in rowBottoms:
+        blockI = bottom['block']
+        botStart = bottom['startDate']
+        botStop = bottom['stopDate']
+        cellTops = []
+        topStarts = set()
+        topStops = set()
+        for top in rowTops:
+            if top['block'] == blockI:
+                cellTops.append(top)
+                topStarts.add(top['startDate'])
+                topStops.add(top['stopDate'])
 
-    posStarts = set()
-    posStops = set()
-    if len(cellTops) > 0:
-        for top in cellTops:
-            posStarts.add(top['startDate'])
-            if top['stopDate'] + DT.timedelta(days=1) < botStop:
-                posStarts.add(top['stopDate'] + DT.timedelta(days=1))
-            posStarts.add(botStart)
-            if top['startDate'] + DT.timedelta(days=-1) > botStart:
-                posStops.add(top['startDate'] + DT.timedelta(days=-1))
-            if top['stopDate'] < botStop:
-                posStops.add(top['stopDate'])
-            posStops.add(botStop)
-        remStarts = sorted(list(posStarts - topStarts))
-        remStops = sorted(list(posStops - topStops))
-        bottom['startDate'] = remStarts[0]
-        bottom['stopDate'] = remStops[0]
-        if len(remStarts) > 1:
-            remStarts.pop(0)
-            for l, split in enumerate(remStarts):
-                splitBottom = {'block' : blockI,
-                            'startDate' : remStarts[l],
-                            'stopDate' : remStops[l],
-                            'rotation' : bottom['rotation']}
-            schedule.append(splitBottom)
+        posStarts = set()
+        posStops = set()
+        if len(cellTops) > 0:
+            for top in cellTops:
+                posStarts.add(top['startDate'])
+                if top['stopDate'] + DT.timedelta(days=1) < botStop:
+                    posStarts.add(top['stopDate'] + DT.timedelta(days=1))
+                posStarts.add(botStart)
+                if top['startDate'] + DT.timedelta(days=-1) > botStart:
+                    posStops.add(top['startDate'] + DT.timedelta(days=-1))
+                if top['stopDate'] < botStop:
+                    posStops.add(top['stopDate'])
+                posStops.add(botStop)
+            remStarts = sorted(list(posStarts - topStarts))
+            remStops = sorted(list(posStops - topStops))
+            bottom['startDate'] = remStarts[0]
+            bottom['stopDate'] = remStops[0]
+            if len(remStarts) > 1:
+                remStarts.pop(0)
+                for l, split in enumerate(remStarts):
+                    splitBottom = {'block' : blockI,
+                                'startDate' : remStarts[l],
+                                'stopDate' : remStops[l],
+                                'rotation' : bottom['rotation']}
+                schedule.append(splitBottom)
 
 # With all those adjustments done, re-sort the schedule list by start Dates
-sortSched = sorted(schedule, key=lambda k: k['startDate'])
+    sortSched = sorted(schedule, key=lambda k: k['startDate'])
 # Finally, put the sorted rotations list into the current resDict
-resDict[AmionName]['schedule'] = sortSched
-# print ''
+    resDict['schedule'] = sortSched
+# And add the whole resident's info to the main dict
+    allRes[AmionName] = resDict
 
-
-print resDict
+    # print resDict
 # print len(resDict[AmionName]['schedule'])
 # for rot in  resDict[AmionName]['schedule']:
-    # print rot
+        # print rot
+# for res in allRes:
+    # print res
+# print allRes['Sun-V']['schedule']
+# for rotation in allRes['Sun-V']['schedule']:
+    # print rotation
+allRotations = set()
+for res in allRes:
+    sched = allRes[res]['schedule']
+    for rot in sched:
+        allRotations.add(rot['rotation'])
+# print allRotations
 
+'''
+Proofread audit some rotation dates to make sure the new parsing works.
+Put some more notes in around the trys & lens and new ifs for block quirks.
+Note the rounding & half issue, that it may not find the right split dates for
+months that don't start on Mondays
 
+Make the parsing thing into a function???
+Super loop to get all the residents.
+Output the list of blocks for Heidi to start on
+Store allRes local.
+
+Start output machines (prolly separate scripts):
+    - Indiv date lookup - given AmionName, date, return rotation. This feeds to
+    podSched
+    - Date lookup + csv. Given same, return rotation & parameter for that day
+    (meal allowance, location, hot/cold score, whatever)
+    - Weekly lookup - esp for conf lookup, given Monday
+'''
